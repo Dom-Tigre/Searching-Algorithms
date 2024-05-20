0dd0b40115d872b9d8ea4bdb507da03802334d17
Dominic Tigreros-Bosini <162226392+Dom-Tigre@users.noreply.github.com>
2024-05-20 12:09:20 +0100

    Created test_searching.py
    Unit tests working

---
+++ test_searching.py
@@ -0,0 +1,28 @@
+"""
+This module contains tests for the searching algorithms learnt in the A-Level CS course
+"""
+
+import unittest
+from searching import Search
+
+
+class TestSearching(Search, unittest.TestCase):
+    """
+    This class contains unit tests for the searching algorithms
+    """
+
+    def test_binary_search(self):
+        """
+        This function tests the binary search algorithm
+        """
+
+        arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        floor = 0
+        ceiling = len(arr) - 1
+        target = 3
+        result = self.binary_search(arr, floor, ceiling, target)
+        expected_result = 2
+        self.assertEqual(result, expected_result)
+
+if __name__ == "__main__":
+    unittest.main()
